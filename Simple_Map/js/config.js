a3c3aac4bda3adb59fe56c26c8dd3b290e46c18a
nxckiii <nishataminsju15@gmail.com>
2022-05-20 23:41:28 -0400

    Simple_Map/js/.gitignore

--- Simple_Map/js/config.js
+++ Simple_Map/js/config.js
@@ -1,2 +1,2 @@
 // API
-const API_KEY = "pk.eyJ1Ijoibnhja2lpaSIsImEiOiJjbDNheHNleXgwMHo5M2JxcnZmZ3VpczE0In0.17btd2tssJ2jIu8PttfpZQ"
+const API_KEY = "pk.eyJ1Ijoibnhja2lpaSIsImEiOiJjbDNheHNleXgwMHo5M2JxcnZmZ3VpczE0In0.17btd2tssJ2jIu8PttfpZQ";
